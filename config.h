7b9ed99d2f7f5efbf938b912910da6dcf2457c6d
jazzymaster1 <141975423+jazzymaster1@users.noreply.github.com>
2023-10-04 12:09:46 -0300

    Update config.h

--- config.h
+++ config.h
@@ -67,7 +67,12 @@ static const struct arg args[] = {
 	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
     //{ netspeed_rx, "%sB/s  ", "enp0s3" },
 //	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
-	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
+// se estiver no notbook e quiser mostrar a bateria, descomente as duas linhas embaixo:
+// ( MUDAR PRA BAT0 CASO /sys/class/power_suply CONTER BAT0 )
+	/*{ battery_state, "%s  ", "BAT1"},
+	{ battery_perc, "%s%%  ", "BAT1"}, */
+
+	{ cpu_perc, "[CPU %s%%]   ", NULL	      },
+	{ ram_perc, "[RAM %s%%]   ", NULL	      },
 	{ datetime, "%s",           "%a %b %d %r" },
 };
